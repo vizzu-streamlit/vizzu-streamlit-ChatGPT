2855a191b9572c1e9b6e73ef61939f78dee84f38
Peter Vidos <peter@vizzuhq.com>
2023-02-01 01:15:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        chart.animate(output)
+        #chart.animate(output)
